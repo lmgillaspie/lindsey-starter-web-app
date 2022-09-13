0b004bd882cf6a8c7d5ca00e7e6dc061aad06e78
Pat L <pat.leong.ctr@swf.army.mil>
2022-09-13 14:39:58 -0500

    Create response entity with location

--- src/main/java/com/example/springboot/Truck.java
+++ src/main/java/com/example/springboot/Truck.java
@@ -5,10 +5,12 @@ import lombok.Getter;
 
 import javax.persistence.*;
 
+import org.springframework.data.domain.AbstractAggregateRoot;
+
 @Getter
 @Builder
 @Entity
-public class Truck {
+public class Truck extends AbstractAggregateRoot<Truck> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/example/springboot/TruckController.java
+++ src/main/java/com/example/springboot/TruckController.java
@@ -1,12 +1,20 @@
 package com.example.springboot;
 
+import java.net.URI;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.springboot.Repositories.TruckRepository;
 import com.example.springboot.Truck.TruckBuilder;
+import com.rabbitmq.client.RpcClient.Response;
 
 @RestController
 @RequestMapping("/trucks")
@@ -16,12 +24,27 @@ public class TruckController {
     private TruckRepository repository;
 
     @PostMapping("/")
-    public Truck create() {
+    public ResponseEntity<Truck> create() {
 
         Truck newTruck = Truck.builder()
                 .status(TruckStatus.ELIGIBLE_FOR_INSPECTION)
                 .build();
 
-        return repository.save(newTruck);
+        newTruck = repository.save(newTruck);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newTruck.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Truck> read(@PathVariable Long id) {
+        Optional<Truck> optionalTruck = repository.findById(id);
+
+        if (optionalTruck.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(optionalTruck.get());
     }
 }
