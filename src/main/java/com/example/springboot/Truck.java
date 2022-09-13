7cd4092aa2b74bb4ba80a4258079f5a75aee3202
Pat L <pat.leong.ctr@swf.army.mil>
2022-09-13 14:18:59 -0500

    Build the truck for post

--- src/main/java/com/example/springboot/Truck.java
+++ src/main/java/com/example/springboot/Truck.java
@@ -1,10 +1,12 @@
 package com.example.springboot;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@Getter
 @Builder
 @Entity
 public class Truck {
--- src/main/java/com/example/springboot/TruckController.java
+++ src/main/java/com/example/springboot/TruckController.java
@@ -1,17 +1,28 @@
 package com.example.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.springboot.Repositories.TruckRepository;
+import com.example.springboot.Truck.TruckBuilder;
+
 @RestController
 public class TruckController {
-    
+
+    @Autowired
+    private TruckRepository repository;
+
     @PostMapping("/")
     public int create() {
 
-        
+        TruckBuilder builder = new TruckBuilder();
 
-        return 1;
+        Truck newTruck = builder.build();
+
+        newTruck = repository.save(newTruck);
+
+        return newTruck.getId();
     }
 
 }
