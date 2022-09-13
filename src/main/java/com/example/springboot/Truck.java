2aea80d3a3d1afc582096113da98e70359265b89
Salman Malik <smalik@pivotal.io>
2022-09-13 14:37:08 -0400

    Adding the required JPA annotations

--- src/main/java/com/example/springboot/Truck.java
+++ src/main/java/com/example/springboot/Truck.java
@@ -2,8 +2,15 @@ package com.example.springboot;
 
 import lombok.Builder;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Builder
+@Entity
 public class Truck {
+
+    @Id
     private int id;
+
     private TruckStatus status;
 }
