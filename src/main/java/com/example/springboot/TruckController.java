5e4ac698800f3c63ae0160291a9716909570ae86
“ChrisF” <cfricke@vmware.com>
2022-09-15 14:32:21 -0400

    trying some cors annotation

--- src/main/java/com/example/springboot/TruckController.java
+++ src/main/java/com/example/springboot/TruckController.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/trucks")
 @RequiredArgsConstructor
 public class TruckController {
